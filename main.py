f929c1a8014b37e87f0a6bb239d23ea324cc8fb9
Ali430yt <69361270+Ali430yt@users.noreply.github.com>
2022-04-10 01:31:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests,os
+from flask import *
+from flask import jsonify
+from apiclient.discovery import build
+app = Flask(__name__)
+@app.route('/v1/api/marko/',methods=['GET'])
+def main():
+    text = str(request.args.get("text"))
+    maxresults = int(request.args.get("maxresults"))
+    key = "AIzaSyC4RYlY8-1-taaTly3VHnccVCGa6WC5nv8"
+    y = build('youtube',"v3",developerKey=key)
+    req = y.search().list(q=text,part="snippet",type="video",maxResults=maxresults)
+    r = req.execute()
+    list_video = []
+    for i in range(maxresults):
+        data = {"video_"+str(i+1):r["items"][i]["snippet"]}
+        list_video.append(data)
+    return jsonify(list_video)
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
